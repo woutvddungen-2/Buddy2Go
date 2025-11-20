3f51eea18b8f8266ba4c2317cf6eb4def3453386
admin <wout.vandendungen@student.fontys.nl>
2025-11-20 22:05:59 +0100

    added colored user bubbles for chat

--- Client/wwwroot/js/scrollHelper.js
+++ Client/wwwroot/js/scrollHelper.js
@@ -1,6 +1,13 @@
 ﻿window.scrollHelper = {
     scrollToEnd: function (element) {
-        if (element)
-            element.scrollTop = element.scrollHeight;
+        if (!element) return;
+        element.scrollTop = element.scrollHeight;
+    },
+
+    isNearBottom: function (element) {
+        if (!element) return true;
+        const threshold = 80; // px from bottom
+        const distance = element.scrollHeight - element.scrollTop - element.clientHeight;
+        return distance < threshold;
     }
 };
