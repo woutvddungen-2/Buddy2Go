da92b88d40d7ebbafb37ff53d36734df30cc10aa
admin <wout.vandendungen@student.fontys.nl>
2025-11-22 13:35:30 +0100

    Re-added respond to pending join request to MyJourneys, this was accidentally deleted in previous split

--- Server/Models/Rating.cs
+++ Server/Models/Rating.cs
@@ -6,7 +6,7 @@
         public int JourneyId { get; set; }
         public Journey Journey { get; set; } = null!;
         public int? UserId { get; set; }
-        public User User { get; set; } = null!;
+        public User? User { get; set; }
         public int RatingValue { get; set; }
         public string? Note { get; set; } = null;
         public DateTime? Created { get; set; } = DateTime.UtcNow;
