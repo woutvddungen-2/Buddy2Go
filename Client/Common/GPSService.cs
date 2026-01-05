001790c30d83c9157c52202621962f87e74046de
admin <wout.vandendungen@student.fontys.nl>
2026-01-05 20:27:32 +0100

    Changed GPSService to use ServiceResult Structure

---
+++ Client/Common/GPSService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.JSInterop;
+
+namespace Client.Common
+{    public class GPSService
+    {
+        private readonly IJSRuntime js;
+        public GPSService(IJSRuntime js)
+        {
+            this.js = js;
+        }
+
+        public async Task<ServiceResult<GeolocationResult>> GetGPSAsync()
+        {
+            try
+            {
+                GeolocationResult data = await js.InvokeAsync<GeolocationResult>("geolocationFunctions.getCurrentPosition");
+                return ServiceResult<GeolocationResult>.Succes(data);
+            }
+            catch (Exception ex)
+            {
+                return ServiceResult<GeolocationResult>.Fail($"Geolocation function error: {ex.Message}");
+            }
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,5 @@
 using Client;
+using Client.Common;
 using Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
